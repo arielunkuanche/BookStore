d6b3c68866a1fa9d07bd706b8f7005eeaa052f3a
Ariel <bhe099@myy.haaga-helia.fi>
2024-02-15 09:05:38 +0200

    REST service added

--- src/main/java/com/bookstore/bookstore/web/BookController.java
+++ src/main/java/com/bookstore/bookstore/web/BookController.java
@@ -1,11 +1,16 @@
 package com.bookstore.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bookstore.bookstore.domain.BookRepository;
 import com.bookstore.bookstore.domain.Category;
@@ -26,6 +31,19 @@ public class BookController {
     public String bookList(Model model){
         model.addAttribute("books", brepository.findAll());
         return "booklist";
+    }
+
+    // restful service to get all books
+    @RequestMapping(value="/books", method=RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest(){
+        return(List<Book>) brepository.findAll();
+    }
+
+    // restful service to get book by id
+    
+    @RequestMapping(value="/book/{id}", method=RequestMethod.GET)
+    public @ResponseBody Optional<Book> findStudentByIdRest(@PathVariable("id") Long bookId){
+        return brepository.findById(bookId);
     }
 
     @RequestMapping(value="/add", method=RequestMethod.GET)
